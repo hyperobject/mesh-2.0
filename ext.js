92fc8ed6ebaa3889295f291afd8d61aea9948a4f
bobbybee <j104@rocketmail.com>
2014-08-11 14:10:24 -0400

    Crude broadcasting functionality

--- ext.js
+++ ext.js
@@ -1,4 +1,6 @@
 (function(ext) {
+	ext.incomingBroadcasts = {};
+	
 	ext._shutdown = function() {
 	
 	};
@@ -6,6 +8,12 @@
 	ext._getStatus = function() {
 		return {status: 2, msg: 'Ready'};
 	};
+	
+	ext.send = function(msg) {
+		if(ext.isOpen) {
+			ext.socket.send(JSON.stringify(msg));
+		}
+	}
 	
 	ext.connect = function(host, port, callback) {
 		ext.socket = new WebSocket("ws://"+host+":"+port+"/", "mesh");
@@ -19,6 +27,10 @@
 		ext.socket.onmessage = function(e) {
 			var message = JSON.parse(e.data);
 			console.log(message);
+			
+			if(message.type == "broadcast") {
+				ext.incomingBroadcasts[message.message] = true;
+			}
 		}
 		
 		ext.socket.onclose = function(e) {
@@ -30,9 +42,28 @@
 		}
 	}
 	
+	ext.broadcast = function(message) {
+		ext.send({
+			type: "broadcast",
+			message: message
+		});
+	}
+	
+	ext.whenIReceive = function(message) {
+		if(ext.incomingBroadcasts[message]) {
+			ext.incomingBroadcasts[message] = false;
+			return true;
+		} 
+		
+		return false;
+	}
+	
 	var descriptor = {
 		blocks: [
-			['w', 'connect to mesh server %s port %n', 'connect', 'localhost', 4354]
+			['w', 'connect to mesh server %s port %n', 'connect', 'localhost', 4354],
+			
+			[' ', 'broadcast %s', 'broadcast', 'message 1'],
+			['h', 'when I receive %s', 'whenIReceive', 'message 1']
 		]
 	};
 	
--- server.js
+++ server.js
@@ -11,10 +11,26 @@ var wss = new WebSocketServer({
 wss.on('connection', function(ws) {
 	console.log("Incoming connection..");
 	
+	function routeMessage(msg, excluding) {
+		// TODO: setup grouping
+		
+		var data = JSON.stringify(msg);
+		
+		for(var i in wss.clients)
+			if(wss.clients[i] != excluding)
+				wss.clients[i].send(data);
+	}
+	
 	ws.on('message', function(message) {
 		message = JSON.parse(message);
 		console.log(message);
 		
+		if(message.type == "broadcast") {
+			routeMessage({
+				type: "broadcast",
+				message: message.message
+			}, ws);
+		}
 	});
 	
 })
